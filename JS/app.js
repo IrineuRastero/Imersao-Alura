4a18210039dea29a4d08df4298eda0f6d018cbbc
Gustavo Dantas <151588742+IrineuRastero@users.noreply.github.com>
2024-09-07 14:42:52 -0300

    other branch bug unintentionally fixed

--- JS/app.js
+++ JS/app.js
@@ -2,4 +2,15 @@
 
 let section = document.getElementById("caixa-resultados")
 
-console.log(section);
+
+for (let dado of aberturas){
+
+section.innerHTML += `
+    <div class="item-resultado">
+                <a href="${dado.link}" target="_blank">
+                    <h2>${dado.nome}</h2>
+                </a>
+                <p>${dado.descricao}</p>
+    </div>
+`
+}
--- JS/dados.js
+++ JS/dados.js
@@ -1,52 +1,12 @@
 const aberturas = [
     {
       nome: "Ruy Lopez",
-      descrição: "Uma das aberturas mais populares, caracterizada pelo movimento inicial e4 e e5, seguido de Cf3 e Cc6.",
-      cor: "brancas"
+      descricao: "Uma das aberturas mais populares, caracterizada pelo movimento inicial e4 e e5, seguido de Cf3 e Cc6.",
+      link: "https://www.youtube.com/watch?v=TyWJJro6Vw8"
     },
     {
       nome: "Gambito da Dama",
-      descrição: "Abertura agressiva em que as brancas sacrificam um peão para obter um rápido desenvolvimento e controle do centro do tabuleiro.",
-      cor: "brancas"
-    },
-    {
-      nome: "Defesa Francesa",
-      descrição: "Abertura sólida e posicional, caracterizada pelos movimentos iniciais e4 e e6.",
-      cor: "brancas"
-    },
-    {
-      nome: "Siciliana",
-      descrição: "Uma das aberturas mais versáteis e populares, com muitas variações, iniciada com e4 e c5.",
-      cor: "brancas"
-    },
-    {
-      nome: "Defesa Caro-Kann",
-      descrição: "Abertura sólida e posicional, similar à Defesa Francesa, mas com o movimento inicial c6 em vez de e6.",
-      cor: "brancas"
-    },
-    {
-        nome: "Gambito do Rei",
-        descrição: "Abertura agressiva em que as brancas sacrificam um peão para obter rápido desenvolvimento e controle do centro, similar ao Gambito da Dama, mas com um plano estratégico diferente.",
-        cor: "brancas"
-      },
-      {
-        nome: "Defesa Alekhine",
-        descrição: "Abertura sólida e posicional, caracterizada pelo movimento inicial e4 e Nf6, seguida de e5.",
-        cor: "pretas"
-      },
-      {
-        nome: "Abertura Inglesa",
-        descrição: "Abertura sólida e posicional, caracterizada pelos movimentos iniciais c4 e c5.",
-        cor: "brancas"
-      },
-      {
-        nome: "Defesa Escandinava",
-        descrição: "Abertura sólida e posicional, caracterizada pelos movimentos iniciais e4 e d5.",
-        cor: "pretas"
-      },
-      {
-        nome: "Abertura Escocesa",
-        descrição: "Abertura flexível e versátil, caracterizada pelos movimentos iniciais e4 e e5, seguida de Cf3 e Nc6.",
-        cor: "brancas"
-      }
+      descricao: "Abertura agressiva em que as brancas sacrificam um peão para obter um rápido desenvolvimento e controle do centro do tabuleiro.",
+      link: "https://www.youtube.com/watch?v=4XqFOq3j99U"
+    }
   ];
