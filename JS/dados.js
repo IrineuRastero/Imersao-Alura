77e4d806504d14bc77b7e39047a7685cac000ae9
Gustavo Dantas <151588742+IrineuRastero@users.noreply.github.com>
2024-09-07 12:57:41 -0300

    Update dados.js

--- JS/dados.js
+++ JS/dados.js
@@ -2,17 +2,17 @@ let Brancas = [
   {
       nome: "Abertura Italiana",
       descricaoCurta: "Uma das aberturas mais populares, caracterizada pelo rápido desenvolvimento dos bispos e o controle do centro.",
-      linkYoutube: "http://www.youtube.com/embed/dv9h6klKCLE"
+      linkYoutube: "https://www.youtube.com/embed/dv9h6klKCLE"
   },
   {
       nome: "Ruy Lopez",
       descricaoCurta: "Uma abertura clássica que envolve um ataque ao cavalo do oponente.",
-      linkYoutube: "http://www.youtube.com/embed/TyWJJro6Vw8"
+      linkYoutube: "https://www.youtube.com/embed/TyWJJro6Vw8"
   },
   {
       nome: "Gambito do Rei",
       descricaoCurta: "Uma abertura agressiva onde as brancas sacrificam um peão para ganhar espaço e iniciativa.",
-      linkYoutube: "http://www.youtube.com/embed/3xqw1J_Rc-A"
+      linkYoutube: "https://www.youtube.com/embed/3xqw1J_Rc-A"
   }
 ];
 
@@ -20,16 +20,16 @@ let Pretas = [
   {
       abertura: "Defesa Siciliana",
       descricao: "Uma abertura muito popular e agressiva, caracterizada pela resposta 1...c5 ao 1.e4 das brancas.",
-      link: "http://www.youtube.com/embed/H5TA2IwkDrk"
+      link: "https://www.youtube.com/embed/H5TA2IwkDrk"
   },
   {
       abertura: "Defesa Francesa",
       descricao: "Uma abertura sólida e posicional, que busca igualdade e contra-ataques.",
-      link: "http://www.youtube.com/embed/bkxUhOjYexM"
+      link: "https://www.youtube.com/embed/bkxUhOjYexM"
   },
   {
       abertura: "Defesa Caro-Kann",
       descricao: "Uma abertura sólida e flexível, que oferece diversas opções estratégicas.",
-      link: "http://www.youtube.com/embed/WYT90WbN_6U"
+      link: "https://www.youtube.com/embed/WYT90WbN_6U"
   }
 ];
